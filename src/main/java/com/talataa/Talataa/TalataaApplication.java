104563afe0799bd5982f45ee0126eaa8289b9186
HarolNoscue <harolocerquera@gmail.com>
2023-05-15 15:42:57 -0500

    preba tecnica Talataa

---
+++ src/main/java/com/talataa/Talataa/TalataaApplication.java
@@ -0,0 +1,13 @@
+package com.talataa.Talataa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalataaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalataaApplication.class, args);
+	}
+
+}
